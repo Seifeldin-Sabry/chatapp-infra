d09bd2d09f1d2ca727408b32705f35e8c7019bdd
Paul Mocanu <paul-cristian.mocanu@student.kdg.be>
2023-05-16 11:33:29 +0200

    tailwind

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 
 defineProps({
   msg: String,
+    warning: String
 })
 
 const count = ref(0)
@@ -10,6 +11,7 @@ const count = ref(0)
 
 <template>
   <h1>{{ msg }}</h1>
+    <h2 class="text-orange-500">{{ warning }}</h2>
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
