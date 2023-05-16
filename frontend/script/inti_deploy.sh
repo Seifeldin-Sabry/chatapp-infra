193677add1c1e0379e9d5a207bc80e444861116e
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2023-05-16 13:21:37 +0200

    updated script

--- frontend/script/inti_deploy.sh
+++ frontend/script/inti_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VM_NAME="VM-$(date +%s)"
+VM_NAME="instance-$(date +%s)"
 REGION="europe-west1"
 ZONE="europe-west1-b"
 MACHINE_TYPE="e2-small"
@@ -8,8 +8,13 @@ IMAGE_FAMILY="ubuntu-2204-lts"
 IMAGE_PROJECT="ubuntu-os-cloud"
 TARGET_TAGS="http-server,ssl-rule-tag,ssh,https-server,default-allow-ssh"
 SQL_INSTANCE_NAME="chatapp"
+DATABASE_NAME="chatapp"
 SQL_ROOT_PASSWORD="chatapp"
 
+if ! pwd | grep scripts 1>/dev/null 2>/dev/null; then
+  echo "Please run this script from the chatapp/client directory"
+  exit 1
+fi
 
 function create_vm() {
   if gcloud compute instances describe "$VM_NAME" --zone="$ZONE" --project="$GOOGLE_PROJECT_ID" --quiet 1>/dev/null 2>/dev/null; then
@@ -27,19 +32,17 @@ function create_vm() {
       apt-get install -y nodejs
       apt-get install -y npm
       apt-get install -y postgresql postgresql-contrib
+      service postgresql start
       "
 }
 
 function authorize_vm_to_instance() {
-#  use gcloud instances list to find the instance, if not exist exit 1
- if ! gcloud sql instances list | grep "$SQL_INSTANCE_NAME" 1>/dev/null 2>/dev/null; then
-    echo "Instance $SQL_INSTANCE_NAME does not exist"
-    create_sql_instance
-  fi
 # Get the list of current authnw
   current_authnw=$(gcloud sql instances describe "$SQL_INSTANCE_NAME" \
     --format='value(settings.ipConfiguration.authorizedNetworks[].value)' \
-    |tr ";" ",")
+    | tr ";" ",");
+
+  echo "Current authnw: ${current_authnw}"
   if [[ -z ${current_authnw} || ${current_authnw} = "0.0.0.0/0" ]]; then
     gcloud sql instances patch "$SQL_INSTANCE_NAME" --authorized-networks="${VM_IP}" -q
   else
@@ -49,25 +52,73 @@ function authorize_vm_to_instance() {
   fi
 }
 
+function get_instance_ip() {
+  echo "Getting VM IP"
+  VM_IP=$(gcloud compute instances describe "$VM_NAME" --format="get(networkInterfaces[0].accessConfigs[0].natIP)" 2>/dev/null)
+  echo "VM IP is $VM_IP"
+}
+
 function create_sql_instance() {
+  if gcloud sql instances describe "$SQL_INSTANCE_NAME" \
+    --quiet 1>/dev/null 2>/dev/null; then
+    echo "SQL instance ${SQL_INSTANCE_NAME} already exists"
+    SQL_INSTANCE_IP=$(gcloud sql instances describe "$SQL_INSTANCE_NAME" \
+      --format="get(ipAddresses[0].ipAddress)" 2>/dev/null)
+    return
+  fi
   echo "Creating SQL instance"
   gcloud sql instances create "$SQL_INSTANCE_NAME" \
     --database-version=POSTGRES_14 \
     --cpu=1 \
     --memory=3840MiB \
     --region="$REGION" \
-    --project="$GOOGLE_PROJECT_ID" \
     --root-password="$SQL_ROOT_PASSWORD" \
-    --storage-type=HDD \
-    --tier=db-f1-micro
+    --storage-type=HDD
+
+  SQL_INSTANCE_IP=$(gcloud sql instances describe "$SQL_INSTANCE_NAME" \
+    --format="get(ipAddresses[0].ipAddress)" 2>/dev/null)
 }
 
 function setup_database() {
   echo "Setting up database"
-  gcloud sql databases create chatapp \
-    --instance="$SQL_INSTANCE_NAME"
-  gcloud sql users create chatapp \
+  if gcloud sql databases describe "$DATABASE_NAME" \
     --instance="$SQL_INSTANCE_NAME" \
-    --password="$SQL_ROOT_PASSWORD"
-  gcloud ssh "$VM_NAME" --zone="$ZONE" --command="
+    --quiet 1>/dev/null 2>/dev/null; then
+    echo "Database ${DATABASE_NAME} already exists"
+    return
+  fi
+  gcloud sql databases create $DATABASE_NAME \
+    --instance="$SQL_INSTANCE_NAME"
+  wait_for_psql
+  gcloud compute scp ./sql/schema.sql "$VM_NAME":~/schema.sql --zone=$ZONE --project=infra3-seifeldin-sabry
+  gcloud compute ssh "$VM_NAME" --project=infra3-seifeldin-sabry --command="echo $SQL_ROOT_PASSWORD | psql -h $SQL_INSTANCE_IP -U postgres -d $DATABASE_NAME -f ~/schema.sql"
 }
+
+function wait_for_psql() {
+  echo "Waiting for psql to start"
+  while ! gcloud compute ssh "$VM_NAME" --project=infra3-seifeldin-sabry --command="which psql"; do
+    echo "psql not yet ready"
+    sleep 1
+  done
+  echo "psql ready"
+}
+
+
+# Function to configure GCloud Storage for static files with open access
+#function configure_gcloud_storage() {
+#  # Create the GCloud Storage bucket
+#  gsutil mb gs://"$BUCKET_NAME"
+#  # Upload static files to the bucket
+#  gsutil -m cp -r "$STATIC_FILES_DIR/*" gs://"$BUCKET_NAME"/
+#  # Set appropriate permissions on the bucket
+#  gsutil -m acl ch -r -u allUsers:R gs://"$BUCKET_NAME"
+#}
+
+# Call the function to configure GCloud Storage for static files
+# configure_gcloud_storage
+
+create_vm
+get_instance_ip
+create_sql_instance
+authorize_vm_to_instance
+setup_database
