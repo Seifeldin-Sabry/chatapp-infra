10657a33df8eef858c5db748cdaeaad6f1cc5447
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2023-05-23 14:02:39 +0200

    changed to relative paths

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,11 @@ const app = express();
 const chatRouter = require('./routes/chatRouter');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
-const cors = require('cors');
-// Middelware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
 
-// app.use(cors());
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use((req, res, next) => {
