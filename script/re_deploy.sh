76cb264c02894cae4a787d4b0165565c0a101daf
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2023-05-23 14:43:58 +0200

    changed to relative paths

--- script/re_deploy.sh
+++ script/re_deploy.sh
@@ -2,8 +2,8 @@
 
 vm_name="chatapp-vm"
 vm_zone="europe-west1-b"
+frontend_service="chatapp-frontend.service"
+backend_service="chatapp-backend.service"
 
-gcloud compute ssh "$vm_name" --zone="$vm_zone" --command="sudo systemctl stop backend.service && sudo systemctl stop frontend.service"
-gcloud compute ssh "$vm_name" --zone="$vm_zone" --command="sudo cd /chatapp-infra && git reset --hard origin/main && git pull origin main"
-gcloud compute ssh "$vm_name" --zone="$vm_zone" --command="sudo systemctl start backend.service && sudo systemctl start frontend.service"
+gcloud compute ssh "$vm_name" --zone="$vm_zone" --command="sudo su && git config --global --add safe.directory /chatapp-infra && systemctl stop $frontend_service && systemctl stop $backend_service && cd /chatapp-infra && git reset --hard origin/main && git pull origin main && systemctl start $backend_service && systemctl start $frontend_service"
 
