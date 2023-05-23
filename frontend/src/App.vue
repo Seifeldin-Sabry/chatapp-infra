712545fe192f30982545d0524dbe0791bd619e27
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2023-05-23 15:23:40 +0200

    changed to relative paths

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -80,7 +80,7 @@ export default defineComponent({
     },
     connectWs(){
       console.log("Starting connection to WebSocket Server")
-      this.connection = new WebSocket("ws://" + window.location.host + "/api?userId=" + this.userId);
+      this.connection = new WebSocket("ws://" + window.location.port + "/api?userId=" + this.userId);
 
 
       // this.connection.on
