b3de2f9ac4325260e897dfb9abc3b366cedf6485
Paul Mocanu <paul-cristian.mocanu@student.kdg.be>
2023-05-23 19:02:56 +0200

    Update dns on OVH with python

--- backend/dnsSetup.py
+++ backend/dnsSetup.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python3
 # Description: This script is used to get the DNS records from OVH API
-import json
 import ovh
 
 # Instanciate an OVH Client.
-# You can generate new credentials with full access to your account on
-# the token creation page
 client = ovh.Client(
-    endpoint='ovh-eu',               # Endpoint of API OVH Europe (List of available endpoints)
-    application_key='cd5b04ffc8157cbf',    # Application Key
-    application_secret='6e079cd046add6c3b1fa8a0ff656c2cb', # Application Secret
-    consumer_key='d575fa8443a3f420a2dc27144f769646',       # Consumer Key
+    endpoint='ovh-eu',  # Endpoint of API OVH Europe (List of available endpoints)
+    application_key='cd5b04ffc8157cbf',  # Application Key
+    application_secret='6e079cd046add6c3b1fa8a0ff656c2cb',  # Application Secret
+    consumer_key='d575fa8443a3f420a2dc27144f769646',  # Consumer Key
 )
 
-result = client.get('/domain'['None'])
-print(client)
+result = client.get("/domain/zone/mocanupaulc.com/record", fieldType='A')
+client.put('/domain/zone/mocanupaulc.com/record/{}'.format(result[0]), target='35.241.221.72',ttl=60)
+client.put('/domain/zone/mocanupaulc.com/record/{}'.format(result[1]), target='35.241.221.72',ttl=60,subDomain='www')
+refresh=client.post('/domain/zone/mocanupaulc.com/refresh')
 
-# Pretty print
-print(result)
-print(json.dumps(result, indent=4))
