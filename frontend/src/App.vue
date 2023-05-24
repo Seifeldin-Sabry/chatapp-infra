07a09e7a2c51f0767b18ad3177e618c15c05f731
Paul Mocanu <paul-cristian.mocanu@student.kdg.be>
2023-05-24 20:51:46 +0200

    try 2

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -85,6 +85,7 @@ export default defineComponent({
           console.log(this.currentContact)
           console.log(new_chat.sender)
           if (this.currentContact === new_chat.sender) {
+            this.currentMessages=Chat.messages
             console.log(this.currentMessages)
             this.currentMessages.unshift(new_chat)
             console.log(this.currentMessages)
