6ba17ba6c078a8a898ab90f23fdf439eacb58a2b
Seifeldin Sabry <seifeldin.sabry@student.kdg.be>
2023-05-25 08:36:14 +0200

    added delete chat functionality

--- script/re_deploy.sh
+++ script/re_deploy.sh
@@ -5,6 +5,9 @@ vm_zone="europe-west1-b"
 frontend_service="chatapp-frontend.service"
 backend_service="chatapp-backend.service"
 project_id="infra3-seifeldin-sabry"
+SQL_ROOT_PASSWORD="chatapp"
+DB_NAME="chatapp"
+SQL_INSTANCE_NAME="chatapp"
 
 # Stop the services
 gcloud compute ssh $vm_name --project $project_id --zone $vm_zone --command "sudo systemctl stop $frontend_service"
@@ -12,6 +15,14 @@ gcloud compute ssh $vm_name --project $project_id --zone $vm_zone --command "sud
 
 # Pull the latest code
 gcloud compute ssh $vm_name --project $project_id --zone $vm_zone --command "cd /chatapp-infra && sudo git reset --hard HEAD && sudo git pull"
+
+# if parameter 1 is passed "db" then update the schema
+if [ "$1" == "db" ]; then
+  internal_ip=$(gcloud compute instances describe $vm_name --project $project_id --zone $vm_zone --format="get(networkInterfaces[0].networkIP)")
+  echo "Internal IP is $internal_ip"
+  gcloud compute ssh $vm_name --project $project_id --zone $vm_zone --command "echo $SQL_ROOT_PASSWORD | sudo -S -h $internal_ip -d $DB_NAME -u postgres psql -f /chatapp-infra/sql/schema.sql"
+fi
+
 
 # Copy the public folder
 gcloud compute ssh $vm_name --project $project_id --zone $vm_zone --command "cd /chatapp-infra && sudo gsutil cp -r gs://chatapp-infra/public ./frontend/public"
